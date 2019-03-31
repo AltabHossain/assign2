29406785d98b563654fefd8d35010d6fe1ee5d17
Md Altab Hossain <altab@Mds-MacBook-Pro.local>
2019-03-31 09:39:38 -0400

    Add, delete, display with php after merging.

--- action_delete.php
+++ action_delete.php
@@ -8,7 +8,6 @@
 
   $sql = mysqli_query ($dbh,$sql);
 
-<<<<<<< HEAD
   if (mysqli_affected_rows() > 0) {
       echo "Records were deleted successfully.";
   }
@@ -16,16 +15,8 @@
       echo "ERROR: No records found with Id $id. " . @mysql_error($dbh);
   }
 ?>
-=======
-    if (@mysql_affected_rows() > 0){
-        echo "Records were deleted successfully.";
-    } else {
-        echo "ERROR: No records found in the database" . @mysql_error($dbh);
-    }
-    ?>
 
  <!-- footer -->
  <?php include('footer.php')?>
 </body>
 </html>
->>>>>>> 0e238d027913a54d40a847b3511c30afae35331a
--- connection.php
+++ connection.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
  // load the values into variables: host, username, passwd, database
 $host='localhost'; // Host name.
 $db_user='root'; // MySQL username.
@@ -14,12 +13,4 @@ if (!$dbh) {
 // select a specific database to access
 mysqli_select_db($dbh, $database) or die('Could not select db');
 //echo "Connected to database <b>$database</b> as user <b>$user</b>.";
-=======
-$host="localhost"; // Host name.
-$db_user="root"; // MySQL username.
-$db_password=""; // MySQL password.
-$database=("techipltd"); // Database name.
-$dbh=@mysql_connect($host,$db_user,$db_password) or die ("Mysql Connection Error");; //Connection to the database
-mysql_select_db($database,$dbh) or die ("Mysql Database Error");
->>>>>>> 0e238d027913a54d40a847b3511c30afae35331a
 ?>
