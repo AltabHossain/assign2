5054650b2db85b7042c15268794299814e4c151d
Mohammad <38024976+sharifikram@users.noreply.github.com>
2019-04-04 13:12:58 -0400

    delete

--- action_delete.php
+++ action_delete.php
@@ -9,14 +9,6 @@
   $sql = mysqli_query ($dbh,$sql);
 
   if (mysqli_affected_rows($dbh) == 1) {
-<<<<<<< HEAD
-      echo "Records were deleted successfully.";
-      include 'delete.php';
-  }
-  else {
-      echo "ERROR: No records found with Id $id.";
-      include 'delete.php';
-=======
       //echo "Records were deleted successfully.";
       //include 'action_display.php';
       //header('location:./delete.php');
@@ -28,7 +20,5 @@
       //echo "ERROR: No records found with Id $id.";
       //header('location:./delete.php');
       echo "<script type='text/javascript'>document.location='./delete.php';</script>";
-
->>>>>>> 9ecf3775f0672f2616d08da7bcd861bc76768a17
   }
 ?>
